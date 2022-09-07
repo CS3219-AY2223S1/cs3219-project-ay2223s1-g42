e6ba3e8923df88fa8884d555eb9adb9ce0ee6763
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-09-08 01:02:45 +0800

    Remove ConfigModule from redisCache module

--- backend/src/cache/redisCache.module.ts
+++ backend/src/cache/redisCache.module.ts
@@ -1,12 +1,11 @@
 import { Module, CacheModule } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigService } from "@nestjs/config";
 import * as redisStore from "cache-manager-ioredis";
 import { RedisCacheService } from "./redisCache.service";
 
 @Module({
   imports: [
     CacheModule.registerAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         store: redisStore,
