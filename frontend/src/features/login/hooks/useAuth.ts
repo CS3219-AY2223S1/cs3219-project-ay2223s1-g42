a3feaee7dcbae4d42f6e2b40090fa6247b7433b3
aidanaden <aidanaden@hotmail.com>
2022-09-11 04:58:00 +0800

    feat: set up useAuth for login

--- frontend/src/features/login/hooks/useAuth.ts
+++ frontend/src/features/login/hooks/useAuth.ts
@@ -1,9 +1,62 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-import { SignInCredentials } from "../../../../../backend/src/utils/zod/credentials";
+import {
+  SignInCredentials,
+  SignUpCredentials,
+} from "../../../../../backend/src/utils/zod/credentials";
+
+type ApiResponse = {
+  message: string;
+};
+
+type User = {
+  id: number;
+  email: string;
+  username: string;
+};
 
 export default function useAuth() {
-  const login = ({ email, password }: SignInCredentials) => {};
+  const getMe = () => {
+    const fetchMe = async () => await axios.get<User>("/users/me");
+    const res = useQuery(["me"], fetchMe);
+    return res;
+  };
 
-  return <div></div>;
+  const refresh = () => {
+    const res = useMutation(
+      async () => await axios.post<ApiResponse>("/auth/refresh")
+    );
+    return res;
+  };
+
+  const signin = ({ email, password }: SignInCredentials) => {
+    const res = useMutation(
+      async () =>
+        await axios.post<ApiResponse>(`/auth/local/signin`, {
+          email,
+          password,
+        })
+    );
+    return res;
+  };
+
+  const signup = ({ email, password, username }: SignUpCredentials) => {
+    const res = useMutation(
+      async () =>
+        await axios.post<ApiResponse>(`/auth/local/signup`, {
+          email,
+          username,
+          password,
+        })
+    );
+    return res;
+  };
+
+  const signout = () => {
+    const res = useMutation(
+      async () => await axios.post<ApiResponse>(`/auth/signout`)
+    );
+    return res;
+  };
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import axios from "axios";
 import type { AppType } from "next/dist/shared/lib/utils";
 
+import { env } from "../env/client.mjs";
 import "../styles/globals.css";
+
+axios.defaults.baseURL = env.NEXT_PUBLIC_API_URL;
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
