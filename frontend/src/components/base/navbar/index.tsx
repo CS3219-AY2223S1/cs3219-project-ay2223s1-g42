4bc73788c1f31ffda6e85c61d27fb646145bab3c
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-09-11 00:09:10 +0800

    feat: navbar in /dashboard

--- frontend/src/components/base/navbar/index.tsx
+++ frontend/src/components/base/navbar/index.tsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function index() {
-  return <div></div>;
+  return (
+    <nav className="w-full bg-gray-750">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="/dashboard">
+              <h2 className="text-2xl font-bold text-white">PeerPrep</h2>
+            </a>
+          </div>
+        </div>
+        <div>
+          <div
+            className={
+              "flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0"
+            }
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-white hover:text-gray-850">
+                <a href="/mmr">Match</a>
+              </li>
+              <li className="text-white hover:text-gray-850">
+                <a href="/me/history">History</a>
+              </li>
+              <li className="text-white hover:text-gray-850">
+                <a href="/questions">Questions</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Navbar from "../components/base/navbar";
+
+export default function login() {
+  return (
+    <div className="flex flex-col justify-between text-gray-100 min-h-screen bg-black">
+      <Navbar></Navbar>
+    </div>
+  );
+}
