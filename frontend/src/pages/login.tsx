8ba100fef45d8df9fe2266b22a4a7267b965c7eb
Ryan Aidan <aidanaden@hotmail.com>
2022-09-10 22:05:57 +0800

    feat: complete frontend pages setup (#24)
    * feat: add prisma db init migration
    
    * feat: add zod dto global pipes
    
    * feat: completed crud + jwt
    
    * feat: add JWT access and refresh tokens via cookies, TODO: add http-only + secure cookies
    
    * feat: add cookie options
    
    * feat: add signin and signup credential dto
    
    * feat: update access token strategy
    
    * chore: move decorator folder
    
    * chore: move zod and decorators to utils folder, remove types from package.json
    
    * feat: update zod types
    
    * feat: add password min length validation
    
    * feat: add helmet, csurf and websocket gateway server
    
    * feat: working websocket nestjs server, added jwt guard
    
    * feat: update cors and websocket
    
    * feat: update frontend for auth/ws testing
    
    * feat: add ws jwt auth
    
    * feat: include credentials in socket.io connection for frontend
    
    * feat: remove .env.local and yarn.lock from main gitignore
    
    * feat: add tailwind directives to global css
    
    * feat: fix tailwind config
    
    * feat:(frontend): add radix components and prep folder structure

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function login() {
+  return (
+    <div className="flex flex-col justify-between text-gray-100 min-h-screen items-center bg-red-500">
+      <div className="w-screen max-w-5xl px-4 flex flex-col mb-12 bg-blue-500 text-center">
+        centered loginsadddddddddddddddddddddddddddddddddddddddd
+        ssssssssssssssssssssssssssssssssscentered
+        loginsadddddddddddddddddddddddddddddddddddddddd
+        sssssssssssssssssssssssssssssssss centered
+        loginsadddddddddddddddddddddddddddddddddddddddd
+        sssssssssssssssssssssssssssssssss centered
+        loginsadddddddddddddddddddddddddddddddddddddddd
+        sssssssssssssssssssssssssssssssss centered
+        loginsadddddddddddddddddddddddddddddddddddddddd
+        sssssssssssssssssssssssssssssssss
+      </div>
+    </div>
+  );
+}
