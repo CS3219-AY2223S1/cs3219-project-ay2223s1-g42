0ec09089ddf995ca59487d975c181e1e6f1cc53b
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-09-11 13:01:22 +0800

    feat: add content to /dashboard

--- frontend/src/components/base/navbar/index.tsx
+++ frontend/src/components/base/navbar/index.tsx
@@ -14,18 +14,36 @@ export default function index() {
         <div>
           <div
             className={
-              "flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0"
+              "text-lg flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0"
             }
           >
-            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-              <li className="text-white hover:text-gray-850">
+            <ul className="text-white items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="hover:text-gray-600">
                 <a href="/mmr">Match</a>
               </li>
-              <li className="text-white hover:text-gray-850">
+              <li className="hover:text-gray-600">
                 <a href="/me/history">History</a>
               </li>
-              <li className="text-white hover:text-gray-850">
+              <li className="hover:text-gray-600">
                 <a href="/questions">Questions</a>
+              </li>
+              <li className="hover:text-gray-600">
+                <div className="p-4">
+                  <div className="group relative">
+                    <button className="bg-gray-800 text-white px-5 h-10 rounded">
+                      Profile
+                    </button>
+                    <nav className="bg-white invisible border-gray-800 rounded w-60 absolute left-0 top-full transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1">
+                      <ul className="py-1">
+                        <li>
+                          <a href="#" className="block px-4 py-2">
+                            Sign out
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -2,10 +2,18 @@ import React from "react";
 
 import Navbar from "../components/base/navbar";
 
-export default function login() {
+export default function dashboard() {
   return (
     <div className="flex flex-col justify-between text-gray-100 min-h-screen bg-black">
-      <Navbar></Navbar>
+      <Navbar />
+      <div className="flex h-screen items-center">
+        <div className="m-auto space-y-10">
+          <h1 className="text-5xl">Welcome to PeerPrep</h1>
+          <button className="outline outline-offset-2 outline-2 rounded-md p-1">
+            Match
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
