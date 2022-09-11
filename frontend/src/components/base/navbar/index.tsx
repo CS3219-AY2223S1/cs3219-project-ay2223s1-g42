edac70cb38b89c1eb544593c635b790a53265abb
Yusuf Bin Musa <29030941+yusufaine@users.noreply.github.com>
2022-09-11 19:21:52 +0800

    feat: FE dashboard (#33)
    * feat: navbar in /dashboard
    
    * feat: add content to /dashboard
    
    * fix: navbar creating unnecessary scroll
    
    * fix: conflict
    
    * feat: install react-step-form package, store navbar links in constant
    
    * feat: add primary button to navbar
    
    Co-authored-by: aidanaden <aidanaden@hotmail.com>

--- frontend/src/components/base/navbar/index.tsx
+++ frontend/src/components/base/navbar/index.tsx
@@ -1,3 +1,63 @@
+import { PrimaryButton } from "../button";
+
+const LINKS = [
+  {
+    href: "/mmr",
+    title: "match",
+  },
+  {
+    href: "/history",
+    title: "history",
+  },
+  {
+    href: "/questions",
+    title: "questions",
+  },
+];
+
 export function Navbar() {
-  return <nav className="sticky h-14 text-lg top-0 z-[45] bg-red-200" />;
+  return (
+    <nav className="sticky h-14 text-lg top-0 z-50 bg-red-200">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="/dashboard">
+              <h2 className="text-2xl font-bold text-white">PeerPrep</h2>
+            </a>
+          </div>
+        </div>
+        <div>
+          <div
+            className={
+              "text-lg flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0"
+            }
+          >
+            <ul className="text-white items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {LINKS.map(({ href, title }) => (
+                <li key={href} className="hover:text-gray-600 capitalize">
+                  <a href={href}>{title}</a>
+                </li>
+              ))}
+              <li className="hover:text-gray-600">
+                <div className="p-4">
+                  <div className="group relative">
+                    <PrimaryButton>Profile</PrimaryButton>
+                    <nav className="bg-red-500 invisible border-gray-800 rounded w-60 absolute left-0 top-full transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1">
+                      <ul className="py-1">
+                        <li>
+                          <a href="#" className="block px-4 py-2">
+                            Sign out
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from "../components/base/navbar";
+
+export default function dashboard() {
+  return (
+    <div className="justify-between text-gray-100 min-h-screen bg-black">
+      <Navbar />
+      <div className="flex h-screen items-center">
+        <div className="m-auto space-y-10">
+          <h1 className="text-5xl">Welcome to PeerPrep</h1>
+          <div className="flex flex-col">
+            <button className="outline outline-offset-2 outline-2 rounded-md p-1">
+              Match
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
