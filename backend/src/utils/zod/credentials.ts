fdbf9981591e746fd8ac3e888e317d5eb44f038c
aidanaden <aidanaden@hotmail.com>
2022-09-14 00:50:47 +0800

    chore: fix rebase pull conflicts

--- backend/src/utils/zod/credentials.ts
+++ backend/src/utils/zod/credentials.ts
@@ -1,7 +1,7 @@
 import * as z from "nestjs-zod/z";
 import { createZodDto } from "nestjs-zod/dto";
 
-import { UserModel } from "../../zod";
+import { UserModel } from "src/zod";
 
 export const SignupSchema = UserModel.pick({
   email: true,
