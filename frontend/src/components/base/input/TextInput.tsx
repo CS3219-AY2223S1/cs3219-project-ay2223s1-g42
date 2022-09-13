e5473836feb94228dc92f8f933fa3781597e3aee
aidanaden <aidanaden@hotmail.com>
2022-09-13 19:18:40 +0800

    feat: add form loogic to login page

--- frontend/src/components/base/input/TextInput.tsx
+++ frontend/src/components/base/input/TextInput.tsx
@@ -1,24 +1,60 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { HTMLProps, InputHTMLAttributes } from "react";
+import cs from "classnames";
 
-type Props = InputHTMLAttributes<HTMLInputElement> & {
+type Props = HTMLProps<HTMLInputElement> & {
   label: string;
+  isError?: boolean;
+  error?: string;
 };
 
-export function TextInput({ label, className, ...other }: Props) {
-  return (
-    <div className="relative">
-      <input
-        type="text"
-        id={"floating_outlined"}
-        className={`block px-2.5 pb-2.5 pt-4 w-full text-md text-neutral-900 bg-transparent border-[1px] border-neutral-900 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-transparent focus:placeholder-neutral-400 ${className}`}
-        {...other}
-      />
-      <label
-        htmlFor="floating_outlined"
-        className="absolute text-sm text-neutral-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-[6px] peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-      >
-        {label}
-      </label>
-    </div>
-  );
-}
+const TextInput = React.forwardRef<HTMLInputElement, Props>(
+  ({ label, className, isError, error, ...other }, ref) => {
+    return (
+      <div>
+        <div className="relative">
+          <input
+            type="text"
+            id={"floating_outlined"}
+            ref={ref}
+            className={cs(
+              "block px-2.5 pb-2.5 pt-4 w-full text-md",
+              "bg-transparent border-[1px] appearance-none focus:outline-none",
+              "focus:ring-0 focus:border-blue-600 peer placeholder-transparent",
+              `focus:placeholder-neutral-400 ${className}`,
+              {
+                "text-neutral-900": !isError,
+                "border-neutral-900": !isError,
+                "border-red-500": isError,
+                "focus:border-red-500": isError,
+              }
+            )}
+            {...other}
+          />
+          <label
+            htmlFor="floating_outlined"
+            className={cs(
+              "absolute text-sm duration-300 transform -translate-y-4 scale-75 text-neutral-500",
+              "top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-[6px] peer-placeholder-shown:top-1/2",
+              "peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2",
+              "peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1",
+              {
+                "peer-focus:text-blue-600": !isError,
+                "peer-focus:text-red-500": isError,
+              }
+            )}
+          >
+            {label}
+          </label>
+        </div>
+        {isError && (
+          <p className="mt-2 text-sm text-red-600 text-start">
+            <span>Oops! </span>
+            {error}
+          </p>
+        )}
+      </div>
+    );
+  }
+);
+
+export { TextInput };
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,41 +1,49 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useAuthStore } from "../login/hooks";
-import { SignInCredentials, SigninCredentialsSchema } from "../login/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQueryClient } from "@tanstack/react-query";
+import cs from "classnames";
+
+import {
+  useAuthStore,
+  SignInCredentials,
+  SigninCredentialsSchema,
+} from "../login";
 import {
   BlueButton,
   PrimaryButton,
-  RedButton,
   Container,
-  SecondaryButton,
   LinkButton,
+  TextInput,
 } from "../components/base";
-
-import GoogleOauth from "../login/components/googleOauth";
-import { TextInput } from "../components/base";
-import GoogleIcon from "../components/icon/GoogleIcon";
-
-type LoginCredentials = {
-  email: string;
-  username: string;
-  password: string;
-};
+import { GoogleIcon } from "../components/icons";
 
 export default function login() {
-  const { register, handleSubmit } = useForm<LoginCredentials>();
-  const onSubmit = handleSubmit((data) => {
-    alert(JSON.stringify(data));
+  const queryClient = useQueryClient();
+
+  // sign in mutations
+  const useSignInMutation = useAuthStore((state) => state.signin);
+  const signinMutation = useSignInMutation({
+    onSuccess: () => queryClient.invalidateQueries(["me"]),
+  });
+  const handleSignin = async (credentials: SignInCredentials) => {
+    signinMutation.mutate(credentials);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<SignInCredentials>({
+    resolver: zodResolver(SigninCredentialsSchema),
   });
 
-  // form state
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const onSubmit = handleSubmit(handleSignin);
 
   return (
     <Container>
-      <div className="w-full px-4 flex flex-col text-center space-y-12 mx-auto">
-        <h1 className="font-display font-bold leading-tight text-5xl text-black-600">
+      <div className="w-full px-4 flex flex-col text-center mx-auto">
+        <h1 className="font-display font-bold leading-tight text-5xl mb-12 text-black-600">
           Welcome.
         </h1>
         <div>
@@ -52,38 +60,35 @@ export default function login() {
             </span>
             <div className="flex-grow border-t border-neutral-400"></div>
           </div>
-          <div className="flex flex-col gap-4">
-            <TextInput
-              label="Email"
-              type="email"
-              placeholder="name@company.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextInput
-              label="Username"
-              type="username"
-              placeholder="Username123"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <TextInput
-              label="Password"
-              type="password"
-              placeholder="Very$ecureP4ssword"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className="flex flex-row-reverse">
-              <LinkButton className="font-light self-end text-neutral-400 hover:border-b-neutral-400">
-                Forget password?
-              </LinkButton>
+          <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+            <div className="flex flex-col gap-5">
+              <TextInput
+                label="Email"
+                type="email"
+                placeholder="name@company.com"
+                isError={!!errors.email?.message}
+                error={errors.email?.message}
+                {...register("email", { required: true })}
+              />
+              <TextInput
+                label="Password"
+                type="password"
+                placeholder="Very$ecureP4ssword"
+                isError={!!errors.password?.message}
+                error={errors.password?.message}
+                {...register("password", { required: true, minLength: 8 })}
+              />
+              <div className="flex flex-row-reverse">
+                <LinkButton className="font-light self-end text-neutral-400 hover:border-b-neutral-400">
+                  Forget password?
+                </LinkButton>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-3">
-          <PrimaryButton onSubmit={onSubmit}>Sign in</PrimaryButton>
-          <LinkButton className="self-center">Sign up</LinkButton>
+            <div className="flex flex-col gap-3">
+              <PrimaryButton type="submit">Sign in</PrimaryButton>
+              <LinkButton className="self-center">Sign up</LinkButton>
+            </div>
+          </form>
         </div>
       </div>
     </Container>
