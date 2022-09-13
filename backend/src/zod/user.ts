2a9e3aa308f93d42c75b31db2a35bd9b19f478a9
aidanaden <aidanaden@hotmail.com>
2022-09-14 00:50:47 +0800

    chore: fix rebase pull conflicts

--- backend/src/zod/user.ts
+++ backend/src/zod/user.ts
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import * as z from "nestjs-zod/z"
 import { createZodDto } from "nestjs-zod/dto"
+=======
+import * as z from "nestjs-zod/z";
+import { createZodDto } from "nestjs-zod/dto";
+>>>>>>> chore: fix rebase pull conflicts
 
 export const UserModel = z.object({
   id: z.number().int(),
@@ -9,7 +14,13 @@ export const UserModel = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   hash: z.string(),
   hashRt: z.string().nullish(),
+<<<<<<< HEAD
 })
 
 export class UserDto extends createZodDto(UserModel) {
 }
+=======
+});
+
+export class UserDto extends createZodDto(UserModel) {}
+>>>>>>> chore: fix rebase pull conflicts
