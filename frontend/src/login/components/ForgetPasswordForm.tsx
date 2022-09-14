c64014fb0201535a1a6a513aab4a96f41c356fe6
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-09-15 01:13:47 +0800

    feat: remove success state and put in one file

--- frontend/src/login/components/ForgetPasswordForm.tsx
+++ frontend/src/login/components/ForgetPasswordForm.tsx
@@ -1,22 +1,17 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { TextInput, PrimaryButton } from "src/components/base";
 import { useAuthStore } from "../hooks";
-import {
-  ForgetPasswordInfo,
-  ForgetPasswordInfoSchema,
-  SuccessFormProps,
-} from "../types";
+import { ForgetPasswordInfo, ForgetPasswordInfoSchema } from "../types";
 
-const ForgetPasswordForm = ({ setSuccess }: SuccessFormProps) => {
+const ForgetPasswordForm = () => {
   // forget password mutation
   const useForgetPasswordMutation = useAuthStore(
     (state) => state.forgetPassword
   );
-  const forgetPasswordMutation = useForgetPasswordMutation({
-    onSuccess: () => setSuccess(true),
-  });
+  const forgetPasswordMutation = useForgetPasswordMutation();
 
   // form setup
   const {
@@ -37,23 +32,32 @@ const ForgetPasswordForm = ({ setSuccess }: SuccessFormProps) => {
 
   return (
     <>
-      <form className="flex flex-col gap-8" onSubmit={onSubmit}>
-        <TextInput
-          label="Email Address"
-          type="email"
-          placeholder="name@company.com"
-          isError={!!errors.email?.message}
-          error={errors.email?.message}
-          {...register("email", { required: true })}
-        />
-        <PrimaryButton
-          className="max-w-3xl"
-          type="submit"
-          isLoading={forgetPasswordMutation.isLoading}
-        >
-          Send Reset Instructions
-        </PrimaryButton>
-      </form>
+      <h1 className="font-bold leading-tight text-4xl mt-0 mb-2 text-black-600 flex flex-col py-5 text-left">
+        Forget Password
+      </h1>
+      {forgetPasswordMutation.isSuccess ? (
+        <h4 className="leading-tight text-1xl text-black-50 flex flex-col text-left">
+          Please check your email for the instructions
+        </h4>
+      ) : (
+        <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+          <TextInput
+            label="Email Address"
+            type="email"
+            placeholder="name@company.com"
+            isError={!!errors.email?.message}
+            error={errors.email?.message}
+            {...register("email", { required: true })}
+          />
+          <PrimaryButton
+            className="max-w-3xl"
+            type="submit"
+            isLoading={forgetPasswordMutation.isLoading}
+          >
+            Send Reset Instructions
+          </PrimaryButton>
+        </form>
+      )}
     </>
   );
 };
--- frontend/src/pages/forget-password.tsx
+++ frontend/src/pages/forget-password.tsx
@@ -1,22 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { ForgetPasswordForm } from "../login/components";
 
 export default function forgetPassword() {
-  const [isSuccess, setSuccess] = useState<boolean>(false);
   return (
     <div className="flex flex-col justify-center min-h-screen items-center">
       <div className="w-screen max-w-lg px-4 flex flex-col mb-12 text-center space-y-4">
-        <h1 className="font-bold leading-tight text-4xl mt-0 mb-2 text-black-600 flex flex-col py-5 text-left">
-          Reset Password
-        </h1>
-        {isSuccess ? (
-          <h4 className="leading-tight text-1xl text-black-50 flex flex-col text-left">
-            Please check your email for the instructions
-          </h4>
-        ) : (
-          <ForgetPasswordForm setSuccess={setSuccess} />
-        )}
+        <ForgetPasswordForm />
       </div>
     </div>
   );
