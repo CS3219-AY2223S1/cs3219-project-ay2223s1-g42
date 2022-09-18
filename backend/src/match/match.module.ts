4fa644ed970f0bb50e4f8a8789df85b0df30254d
aidanaden <aidanaden@hotmail.com>
2022-09-19 01:54:09 +0800

    chore: remove unused

--- backend/src/match/match.module.ts
+++ backend/src/match/match.module.ts
@@ -1,7 +1,5 @@
 import { Module } from "@nestjs/common";
 
-import { RedisCacheModule } from "src/cache/redisCache.module";
-import { RoomServiceModule } from "src/room/room.service.module";
 import { MatchGateway } from "./match.gateway";
 import { MatchServiceModule } from "./match.service.module";
 
