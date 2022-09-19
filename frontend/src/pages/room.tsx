eeb928c9918bfb7a5b18f1be4dd032d8883e0cbd
aidanaden <aidanaden@hotmail.com>
2022-09-20 02:49:50 +0800

    chore: remove unused imports from room page

--- frontend/src/pages/room.tsx
+++ frontend/src/pages/room.tsx
@@ -2,7 +2,6 @@ import { NextPage } from "next";
 import dynamic from "next/dynamic";
 
 const DynamicEditor = dynamic(() => import("src/room"), { ssr: false });
-
 const RoomPage: NextPage = () => {
   return (
     <div className="bg-yellow-50 w-full h-screen">
