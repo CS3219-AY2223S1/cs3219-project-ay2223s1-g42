00accadc869b903fd59c41674c86fcc979bad57c
aidanaden <aidanaden@hotmail.com>
2022-09-28 22:50:52 +0800

    feat: add top padding to container

--- frontend/src/components/layout/Container.tsx
+++ frontend/src/components/layout/Container.tsx
@@ -7,7 +7,8 @@ const Container = ({
 }: HtmlHTMLAttributes<HTMLDivElement>) => {
   return (
     <div
-      className={`flex max-w-xl justify-center items-center px-4 mx-auto min-h-screen ${className}`}
+      className={`flex max-w-xl justify-center items-center px-4
+      mx-auto pt-[76px] md:pt-16 min-h-screen ${className}`}
       {...other}
     >
       {children}
