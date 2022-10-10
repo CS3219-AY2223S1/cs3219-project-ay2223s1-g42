981baba975af5d0cc5c9d647222beed78680bcb0
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-10-11 01:41:40 +0800

    fix sign up page

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { BigHeading } from "src/components";
-import { SignupForm } from "../features/login";
+import { useGlobalStore } from "src/store";
+import { SignupForm } from "src/features/login";
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+  const user = useGlobalStore((state) => state.user);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
   return (
     <div className="mx-auto flex w-full flex-col px-4 text-center">
       <BigHeading className="mt-4 mb-12">Welcome.</BigHeading>
