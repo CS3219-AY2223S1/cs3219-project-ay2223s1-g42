9c38249dad39b19617dfa6bb952bbc576e3559ac
blaketingyu <blaketantingyu@gmail.com>
2022-10-11 21:53:45 +0800

    chore: remove comments

--- frontend/src/components/base/button/black.tsx
+++ frontend/src/components/base/button/black.tsx
@@ -13,5 +13,3 @@ export function BlackButton({ className = "", children, ...other }: Props) {
     </BaseButton>
   );
 }
-//text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2
-//hover:border-black-700 hover:bg-black-700 border-black bg-black text-neutral-50 ${className}
