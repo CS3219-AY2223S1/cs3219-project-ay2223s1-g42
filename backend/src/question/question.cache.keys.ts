c4e751b64516a988fc952769aba8a8fc8b4f88b6
blaketingyu <blaketantingyu@gmail.com>
2022-10-26 16:31:53 +0800

    feat: cache the question services for faster retrieval

---
+++ backend/src/question/question.cache.keys.ts
@@ -0,0 +1,19 @@
+const question_summaries_formatted = "summaries_formatted";
+const question_summaries = "summaries";
+const question_contents = "contents";
+const question_topics = "topics";
+const question_qotd_content = "question_of_the_day_content";
+const question_qotd_summary = "question_of_the_day_summary";
+const question_summary_difficulties = "question_summary_difficulties";
+const question_summaries_for_topics = "question_summary_for_topics";
+
+export {
+  question_summaries,
+  question_contents,
+  question_topics,
+  question_qotd_content,
+  question_qotd_summary,
+  question_summary_difficulties,
+  question_summaries_formatted,
+  question_summaries_for_topics,
+};
