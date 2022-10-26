6c47ec87a2d64627315e90a7d1726b8f7eaa60db
blaketingyu <blaketantingyu@gmail.com>
2022-10-26 19:02:07 +0800

    fix: remove unnecessary file and questionservice provider

--- backend/src/question/question.module.ts
+++ backend/src/question/question.module.ts
@@ -3,11 +3,9 @@ import { Module } from "@nestjs/common";
 import { QuestionService } from "./question.service";
 import { QuestionController } from "./question.controller";
 import { RedisCacheModule } from "../cache/redisCache.module";
-import { CronService } from "./questions.cron.service";
-
 @Module({
   controllers: [QuestionController],
-  providers: [QuestionService, CronService],
+  providers: [QuestionService],
   exports: [QuestionService],
   imports: [RedisCacheModule],
 })
