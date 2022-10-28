dc2a86aa7642c59c843e27cf9a24cf34c0a253da
Blake Tan <77196632+blaketingyu@users.noreply.github.com>
2022-10-28 22:07:53 +0800

    perf: cache questions in the backend (#114)
    * feat: cache the question services for faster retrieval
    
    * feat: cache retrieval of summaries via topic
    
    * feat: add cronjob for invalidation of questions in cache
    
    * fix: remove unnecessary file and questionservice provider
    
    * Update backend/src/question/question.service.ts
    
    Co-authored-by: Yusuf Bin Musa <29030941+yusufaine@users.noreply.github.com>
    
    * fix: update based on comments
    
    * feat: cache the question services for faster retrieval
    
    * feat: cache retrieval of summaries via topic
    
    * feat: add cronjob for invalidation of questions in cache
    
    * fix: remove unnecessary file and questionservice provider
    
    * Update backend/src/question/question.service.ts
    
    Co-authored-by: Yusuf Bin Musa <29030941+yusufaine@users.noreply.github.com>
    
    * fix: update based on comments
    
    * test: test the question summaries endpoint
    
    * fix: fix error in getSummariesFromTopicTags - yusuf
    
    * Update backend/src/question/question.service.ts
    
    Co-authored-by: Yusuf Bin Musa <29030941+yusufaine@users.noreply.github.com>
    
    * refactor: remove commented code
    
    * feat: cache the question services for faster retrieval
    
    * feat: cache retrieval of summaries via topic
    
    * feat: add cronjob for invalidation of questions in cache
    
    * fix: remove unnecessary file and questionservice provider
    
    * Update backend/src/question/question.service.ts
    
    Co-authored-by: Yusuf Bin Musa <29030941+yusufaine@users.noreply.github.com>
    
    * fix: update based on comments
    
    * test: test the question summaries endpoint
    
    * feat: cache the question services for faster retrieval
    
    * fix: fix error in getSummariesFromTopicTags - yusuf
    
    * refactor: remove commented code
    
    * chore: code cleanup
    
    * fix: revert change to swagger enum
    
    * refactor: applied PR suggestions (#115)
    
    * feat: add invalidation endpoint
    
    Co-authored-by: Yusuf Bin Musa <29030941+yusufaine@users.noreply.github.com>
    Co-authored-by: Yusuf Bin Musa <yusuf.musa3@gmail.com>

---
+++ backend/src/question/question.cache.keys.ts
@@ -0,0 +1,19 @@
+const QUESTION_CONTENT = "contents";
+const QUESTION_QOTD_CONTENT = "question_of_the_day_content";
+const QUESTION_QOTD_SLUG = "question_of_the_day_slug";
+const QUESTION_QOTD_SUMMARY = "question_of_the_day_summary";
+const QUESTION_SUMMARIES = "summaries";
+const QUESTION_SUMMARIES_FORMATTED = "summaries_formatted";
+const QUESTION_TOPICS = "topics";
+const QUESTION_TITLES = "titles";
+
+export {
+  QUESTION_CONTENT,
+  QUESTION_QOTD_CONTENT,
+  QUESTION_QOTD_SLUG,
+  QUESTION_QOTD_SUMMARY,
+  QUESTION_SUMMARIES,
+  QUESTION_SUMMARIES_FORMATTED,
+  QUESTION_TOPICS,
+  QUESTION_TITLES,
+};
