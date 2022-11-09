15370cf044b16d17c639b83b719e878ca83e2ae3
aidanaden <aidanaden@hotmail.com>
2022-11-09 09:08:23 +0800

    feat: remove user check for question preview page

--- frontend/src/pages/question/index.tsx
+++ frontend/src/pages/question/index.tsx
@@ -1,23 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { GetSummariesResponse } from "shared/api";
-import { LoadingLayout, UnauthorizedPage } from "src/components";
+import { LoadingLayout } from "src/components";
 import { LoadedRoom } from "src/features";
-import { useGlobalStore } from "src/store";
 import { Axios } from "src/services";
 
 const QuestionPage = (): JSX.Element => {
-  const user = useGlobalStore((state) => state.user);
-
   const questionSummaries = useQuery(["all-question-summaries"], () => {
     return Axios.get<GetSummariesResponse>(`/question/summary`).then(
       (res) => res.data
     );
   });
-
-  if (!user) {
-    return <UnauthorizedPage />;
-  }
 
   if (questionSummaries.data) {
     return <LoadedRoom questionSummaries={questionSummaries.data} />;
